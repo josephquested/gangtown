d0961af793a3ff8faf44ecd7724cd590abecbd07
josephquested <josephquested@gmail.com>
2016-07-25 18:58:02 +1200

    initial commit

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+	Movement movement;
+	// public Weapon weapon;
+
+	void Awake ()
+	{
+		movement = GetComponent<Movement>();
+	}
+
+	public void RecieveMovementInput (float horizontal, float vertial)
+	{
+		movement.Move(horizontal, vertial);
+	}
+
+	public void RecieveFireInput (bool fire1)
+	{
+		// if (fire1)
+		// {
+		// 	weapon.RecieveFireInput();
+		// }
+	}
+}
