a96836f33d75a9d4fd3954fa359a20047c0fbda4
josephquested <josephquested@gmail.com>
2016-07-27 13:24:04 +1200

    add pickup interaction trigger

---
+++ Assets/Scripts/PickupCollider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PickupCollider : MonoBehaviour
+{
+	public List<GameObject> pickups = new List<GameObject>();
+
+	void OnTriggerEnter (Collider collider)
+	{
+		if (collider.tag == "Pickup")
+		{
+			pickups.Add(collider.gameObject);
+		}
+	}
+
+	void OnTriggerExit (Collider collider)
+	{
+		if (collider.tag == "Pickup")
+		{
+			pickups.Remove(collider.gameObject);
+		}
+	}
+}
